e31662d71a238a8dc7863bb99c74d2cd848c1499
Kirill Chokparov <kchokparov@gmail.com>
2023-05-16 12:45:05 +0300

    Inheritance example

---
+++ src/main/java/ru/javatutor/inheritance/InheritanceAnimal.java
@@ -0,0 +1,36 @@
+package ru.javatutor.inheritance;
+
+public class InheritanceAnimal {
+    public static void main(String[] args) {
+        Rex rex = new Rex();
+        rex.feed(10, FoodType.MEAT);
+        rex.feed(10, FoodType.GRASS);
+    }
+
+}
+
+abstract class Animal {
+    public abstract boolean feed(int amount, FoodType foodType);
+}
+
+abstract class Predator extends Animal {
+
+    @Override
+    public boolean feed(int amount, FoodType foodType) {
+        if (foodType.equals(FoodType.MEAT)) {
+            // implementation of an abstract method
+            System.out.println("Predator is eating");
+            return true;
+        }
+        System.out.println("Predator doesn't eat such meat");
+        return false;
+    }
+}
+
+class Rex extends Predator {
+
+}
+
+enum FoodType {
+    MEAT, GRASS
+}
